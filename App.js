c3ef66a4f159ccb5cd5760153c8bba405756fd7e
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-01-06 20:23:28 +0500

    post props

--- App.js
+++ App.js
@@ -3,14 +3,18 @@ import {View, Text, StyleSheet, StatusBar, SafeAreaView} from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
 import {Home} from './src/screens';
 import {Post} from './src/components';
+import feed from './src/assets/data/feed';
 
 const App = () => {
+  const post1 = feed[0];
+  const post2 = feed[2];
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         {/* <Home /> */}
-        <Post />
+        <Post post={post2} />
       </SafeAreaView>
     </>
   );
--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -4,35 +4,37 @@ import {View, Text, Image} from 'react-native';
 import styles from './styles';
 
 // create a component
-const Post = () => {
+const Post = props => {
+  const post = props.post;
+
   return (
     <View style={styles.container}>
       {/* Image */}
       <Image
         source={{
-          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+          uri: post.image,
         }}
         style={styles.image}
       />
 
       {/* Bed & Bedroom */}
-      <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+      <Text style={styles.bedrooms}>
+        {post.bed} bed {post.bedroom} bedroom
+      </Text>
 
       {/* Type & Description */}
       <Text style={styles.description} numberOfLines={2}>
-        Entire Flat. Puerto de la cruz lrmLorem Ipsum is simply dummy text of
-        the printing and typesetting industry. Lorem Ipsum has been the
-        industry's standard dummy
+        {post.type}. {post.title}
       </Text>
 
       {/* Old Price &  new Price */}
       <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>$36 </Text>
-        <Text style={styles.price}> $30 </Text> / night
+        <Text style={styles.oldPrice}>${post.oldPrice} </Text>
+        <Text style={styles.price}> ${post.newPrice}</Text> / night
       </Text>
 
       {/* Total Price */}
-      <Text style={styles.totalPrice}>$230 total</Text>
+      <Text style={styles.totalPrice}>${post.totalPrice} total</Text>
     </View>
   );
 };
