b3699bd3a35316a9e86e303ee758adc0b5eb40a9
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-01-06 19:07:01 +0500

    render post

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import {View, Text, StyleSheet, StatusBar, SafeAreaView} from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
 import {Home} from './src/screens';
+import {Post} from './src/components';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Home />
+        {/* <Home /> */}
+        <Post />
       </SafeAreaView>
     </>
   );
